dac2671d72cee282a602d17d98c63c09a93a6ad4
Mac <gg392811726@icloud.com>
2016-12-15 22:46:05 +0800

    swift3.0语法

---
+++ Swift3.0 pragma/Swift3.0 pragma/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Swift3.0 pragma
+//
+//  Created by Yioks-Mac on 16/9/12.
+//  Copyright © 2016年 Yioks-Mac. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var arr : [String];
+print(arr);
+
+
+
